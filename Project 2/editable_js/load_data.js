a55e096683ca7708d3393e70cc12fb1c567791af
komcgeehan <komcg@umd.edu>
2026-04-27 22:23:27 -0400

    project 2

--- Project 2/editable_js/load_data.js
+++ Project 2/editable_js/load_data.js
@@ -7,12 +7,12 @@ async function loadData() {
     // Load your data here by passing a string to the Fetch request.
     // It should be in data.json in the root folder, but you'll need to look at the results to see what's there.
 
-    const response = await fetch ('')  // go get some data
+    const response = await fetch ('./data.json')  // go get some data
     const data = await response.json();
     console.log("data loaded", data);
     
     // You'll need to look at that data in the console to make sure you have restaurants to work with
-    return data;
+    return data. features.map(feature => feature.properties);
   } catch (error) {
     console.error("Failed to load data:", error);
     throw new Error("Could not load data from API");
