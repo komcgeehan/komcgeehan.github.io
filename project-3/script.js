ba17c9d959ee6d382010cea6a3e4560db940c495
komcgeehan <komcg@umd.edu>
2026-05-04 16:36:53 -0400

    Update script.js

--- project-3/script.js
+++ project-3/script.js
@@ -28,7 +28,7 @@ if (document.querySelector('.hero-title')) {
   
     setTimeout(function() {
     new Typed('#typed-text', {
-        strings: ['Reader. Writer. Oxford Comma Enthusiast.'],
+        strings: ['Writer. Reader. Oxford Comma Enthusiast.'],
         typeSpeed: 60,
         backSpeed: 30,
         backDelay: 2000,
